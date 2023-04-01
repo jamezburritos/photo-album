0c045121af6513883718415de834ba2936198ab5
James Barret <jamesbarret715@gmail.com>
2023-04-01 11:49:51 +0200

    added image popup

--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -1,13 +1,36 @@
 <script lang="ts">
 	import '@fontsource/inter'
 	import type { PageData } from './$types'
-	import { Client, Storage } from 'appwrite'
+	import { Client, Query, Storage } from 'appwrite'
+	import { crossfade } from 'svelte/transition'
+	import { quintOut } from 'svelte/easing'
 
 	export let data: PageData
 
-	// dynamically set number of cols in image grid
+	// dynamically set number of columns in image grid
 	let innerWidth: number
 	$: columns = Math.floor(innerWidth / 350)
+
+	// selected image popup
+	let selected = -1
+
+	const [send, receive] = crossfade({
+		duration: (d) => Math.sqrt(d * 200),
+
+		fallback(node) {
+			const style = getComputedStyle(node)
+			const transform = style.transform === 'none' ? '' : style.transform
+
+			return {
+				duration: 600,
+				easing: quintOut,
+				css: (t) => `
+                transform: ${transform} scale(${t});
+                opacity: ${t}
+                `
+			}
+		}
+	})
 
 	// fetch images from storage bucket
 	const client = new Client()
@@ -15,10 +38,13 @@
 		.setProject(data.projectID)
 
 	const storage = new Storage(client)
-	const images = storage.listFiles(data.bucketID)
+	const images = storage.listFiles(data.bucketID, [Query.limit(100)])
+	// TODO: add 'load mode' functionality
 
-	const fetchImage = (fileID: string) => {
-		return storage.getFileView(data.bucketID, fileID).toString()
+	const fetchImage = (fileID: string, quality: number) => {
+		const url = storage.getFileView(data.bucketID, fileID)
+		url.searchParams.set('quality', `${quality}`)
+		return url.toString()
 	}
 </script>
 
@@ -33,15 +59,42 @@
 			<figure class="gallery-item gallery-placeholder" />
 		{/each}
 	{:then images}
-		{#each images.files as file (file.$id)}
+		{#each images.files as file, i (file.$id)}
 			<img
 				class="gallery-item"
 				alt={file.name}
-				src={fetchImage(file.$id)}
+				src={fetchImage(file.$id, 25)}
+				in:receive={{ key: file.$id }}
+				out:send={{ key: file.$id }}
+				on:click={() => (selected = i)}
+				on:keypress={() => (selected = i)}
 			/>
 		{/each}
 	{/await}
 </div>
+
+{#await images then images}
+	{#if selected !== -1}
+		<div
+			id="image-container"
+			on:click={() => {
+				selected = -1
+			}}
+			on:keypress={() => {
+				undefined
+			}}
+		>
+			<figure id="image-focused">
+				<img
+					alt={images.files[selected].name}
+					in:receive={{ key: images.files[selected].$id }}
+					out:send={{ key: images.files[selected].$id }}
+					src={fetchImage(images.files[selected].$id, 100)}
+				/>
+			</figure>
+		</div>
+	{/if}
+{/await}
 
 <style lang="scss">
 	#gallery {
@@ -60,6 +113,34 @@
 			margin: 0 0 8px 0;
 
 			background: linear-gradient(45deg, #bbb, #ccc);
+		}
+	}
+
+	#image-container {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		z-index: 100;
+
+		background: rgba(#333, 0.5);
+
+		#image-focused {
+			width: 80%;
+			height: 100%;
+			max-width: 900px;
+			margin: auto;
+
+			display: block;
+
+			img {
+				width: 100%;
+				height: 100%;
+
+				object-fit: contain;
+				object-position: center;
+			}
 		}
 	}
 </style>
