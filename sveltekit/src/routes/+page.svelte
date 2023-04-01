5624eb3a2d4131ec84ad732c4e994571d88ca54b
James Barret <jamesbarret715@gmail.com>
2023-04-01 17:37:12 +0200

    implementation of batched image loading

--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -1,95 +1,111 @@
 <script lang="ts">
-	import '@fontsource/inter'
 	import type { PageData } from './$types'
-	import { Client, Query, Storage } from 'appwrite'
-	import { crossfade } from 'svelte/transition'
+	import { scale } from 'svelte/transition'
 	import { quintOut } from 'svelte/easing'
+	import { Client, Query, Storage, type Models } from 'appwrite'
 
+	// server-side page data
 	export let data: PageData
 
-	// dynamically set number of columns in image grid
-	let innerWidth: number
-	$: columns = Math.floor(innerWidth / 350)
+	// page bindings
+	let windowWidth: number
+	let windowHeight: number
+	let galleryHeight: number
+	let scrollY: number
 
-	// selected image popup
-	let selected = -1
-
-	const [send, receive] = crossfade({
-		duration: (d) => Math.sqrt(d * 200),
-
-		fallback(node) {
-			const style = getComputedStyle(node)
-			const transform = style.transform === 'none' ? '' : style.transform
-
-			return {
-				duration: 600,
-				easing: quintOut,
-				css: (t) => `
-                transform: ${transform} scale(${t});
-                opacity: ${t}
-                `
-			}
-		}
-	})
-
-	// fetch images from storage bucket
+	// appwrite server access
 	const client = new Client()
 		.setEndpoint('http://localhost/v1')
 		.setProject(data.projectID)
 
 	const storage = new Storage(client)
-	const images = storage.listFiles(data.bucketID, [Query.limit(100)])
-	// TODO: add 'load mode' functionality
 
-	const fetchImage = (fileID: string, quality: number) => {
+	// dynamically set number of columns in image grid
+	$: columns = Math.floor(windowWidth / 350)
+
+	// selected image for modal
+	let selected: Models.File | null
+
+	// number of images in the bucket, used for batched loading
+	let total = 0
+	let loaded = 0
+
+	// fetch a batch of image data from the server, updating counts as needed
+	const fetchImageBatch = async (count: number): Promise<Models.File[]> => {
+		const res = await storage.listFiles(data.bucketID, [
+			Query.limit(count),
+			Query.offset(loaded)
+		])
+
+		total = res.total
+		loaded += res.files.length
+
+		return res.files
+	}
+
+	// fetch the raw image url from the server, at a fixed quality
+	const fetchImage = (fileID: string, quality: number): string => {
 		const url = storage.getFileView(data.bucketID, fileID)
 		url.searchParams.set('quality', `${quality}`)
 		return url.toString()
 	}
+
+	// batches of image urls
+	let batches = [fetchImageBatch(25)]
+
+	// load more images when user approaches the bottom of the page
+	$: {
+		if (windowHeight + scrollY >= galleryHeight * 0.8 && loaded !== total) {
+			batches = [...batches, fetchImageBatch(25)]
+		}
+	}
 </script>
 
-<svelte:window bind:innerWidth />
+<svelte:window
+	bind:innerWidth={windowWidth}
+	bind:innerHeight={windowHeight}
+	bind:scrollY
+/>
 
 <div
 	id="gallery"
-	style:columns
+	style:grid-template-columns="repeat({columns}, 1fr)"
+	bind:offsetHeight={galleryHeight}
 >
-	{#await images}
-		{#each new Array(10) as _}
-			<figure class="gallery-item gallery-placeholder" />
-		{/each}
-	{:then images}
-		{#each images.files as file, i (file.$id)}
-			<img
-				class="gallery-item"
-				alt={file.name}
-				src={fetchImage(file.$id, 25)}
-				in:receive={{ key: file.$id }}
-				out:send={{ key: file.$id }}
-				on:click={() => (selected = i)}
-				on:keypress={() => (selected = i)}
-			/>
-		{/each}
-	{/await}
+	{#each batches as batch}
+		{#await batch}
+			{#each new Array(25) as _}
+				<figure class="gallery-item gallery-placeholder" />
+			{/each}
+		{:then images}
+			{#each images as file (file.$id)}
+				<img
+					class="gallery-item"
+					alt={file.name}
+					src={fetchImage(file.$id, 25)}
+					loading="lazy"
+					on:click={() => (selected = file)}
+					on:keypress={() => (selected = file)}
+				/>
+			{/each}
+		{/await}
+	{/each}
 </div>
 
-{#await images then images}
-	{#if selected !== -1}
+{#await batches then images}
+	{#if selected}
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
 		<div
 			id="image-container"
 			on:click={() => {
-				selected = -1
-			}}
-			on:keypress={() => {
-				undefined
+				selected = null
 			}}
 		>
 			<figure id="image-focused">
 				<img
-					alt={images.files[selected].name}
-					in:receive={{ key: images.files[selected].$id }}
-					out:send={{ key: images.files[selected].$id }}
-					src={fetchImage(images.files[selected].$id, 100)}
+					alt={selected.name}
+					src={fetchImage(selected.$id, 100)}
+					transition:scale={{ easing: quintOut, duration: 200 }}
 				/>
 			</figure>
 		</div>
@@ -98,6 +114,7 @@
 
 <style lang="scss">
 	#gallery {
+		display: grid;
 		/* columns set dynamically */
 		gap: 8px;
 
